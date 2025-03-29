5b290d8db9a3e984007273b9b9c08bd7a32df3e8
Gagan Vasudev <mvgagan15@gmail.com>
2025-03-29 15:46:17 +0530

    solve 1773. Count Items Matching a Rule

---
+++ 1700-1799/1773.Count Items Matching a Rule/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int ans = 0;
+        int index = -1;
+        if (ruleKey.equals("type")) {
+            index = 0;
+        } else if (ruleKey.equals("color")) {
+            index = 1;
+        } else {
+            index = 2;
+        }
+        for (List<String> list : items) {
+            if (list.get(index).equals(ruleValue)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
