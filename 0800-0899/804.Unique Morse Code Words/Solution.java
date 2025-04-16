df1dfda53b3221260743553f9da44df58fec2b76
Gagan Vasudev <mvgagan15@gmail.com>
2025-04-16 23:15:31 +0530

    solve 804. Unique Morse Code Words

---
+++ 0800-0899/804.Unique Morse Code Words/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] arr = new String[] {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        Set<String> set = new HashSet<>();
+        for (String str: words) {
+            StringBuilder sb = new StringBuilder();
+            for (char c: str.toCharArray()) {
+                sb.append(arr[c - 'a']);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
