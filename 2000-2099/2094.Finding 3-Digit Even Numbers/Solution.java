a2504247f39d0f4e12754e3eaef4d19e15a0b4b4
Gagan Vasudev <mvgagan15@gmail.com>
2025-05-12 22:38:16 +0530

    solve 2094. Finding 3-Digit Even Numbers

---
+++ 2000-2099/2094.Finding 3-Digit Even Numbers/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] findEvenNumbers(int[] digits) {
+        List<Integer> list = new ArrayList<>();
+        int[] arr = new int[10];
+        for (int digit: digits) {
+            arr[digit]++;
+        }
+        for (int i = 100; i < 999; i += 2) {
+            if (helper (i, arr)) {
+                list.add(i);
+            }
+        }
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
+    }
+
+    private boolean helper (int num, int[] arr) {
+        int[] temp = new int[10];
+        for (char c: String.valueOf(num).toCharArray()) {
+            temp[Integer.parseInt(String.valueOf(c))]++;
+        }
+        for (int i = 0; i < 10; i++) {
+            if (temp[i] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
